96f68804efe1a2df14a181adc6654bc6774df0c0
Frank Berger <fberger@b-factor.de>
2016-03-23 16:25:58 +0100

    Dropdown element

--- Classes/ViewHelpers/Collection/GetViewHelper.php
+++ Classes/ViewHelpers/Collection/GetViewHelper.php
@@ -16,25 +16,28 @@ class GetViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper 
      * @param mixed $data data
      * @param string $key key name
      * @param string $as variable name
-     * @return array
-     *
-     * @throws nothing
-     *
+     * @param bool $direct return directly
+     * @return string
+     * @throws \TYPO3\CMS\Fluid\Core\ViewHelper\Exception
+     * @throws \TYPO3\CMS\Fluid\Core\ViewHelper\Exception\InvalidVariableException
      */
-    public function render( $data, $key, $as='value') {
+    public function render( $data, $key, $as='value',$direct=false) {
         if (is_object($data) && !method_exists($data,'toArray')) {
             throw new \TYPO3\CMS\Fluid\Core\ViewHelper\Exception('GetViewHelper only supports arrays and objects implementing a toArray Method', 1248728393);
         }
         if (is_object($data)) $data = $data->toArray();
         //$elem = array_shift($data);
 
-
-        $value = $data[$key];
-        $renderChildrenClosure =  $this->buildRenderChildrenClosure();
-        $templateVariableContainer = $this->renderingContext->getTemplateVariableContainer();
-        $templateVariableContainer->add($as,$value);
-        $output = $renderChildrenClosure();
-        $templateVariableContainer->remove($as);
+        if (!isset($data[$key])) return null;
+        if ($direct) return $data[$key];
+        $output = '';
+        if ($value = $data[$key]) {
+            $renderChildrenClosure = $this->buildRenderChildrenClosure();
+            $templateVariableContainer = $this->renderingContext->getTemplateVariableContainer();
+            $templateVariableContainer->add($as, $value);
+            $output = $renderChildrenClosure();
+            $templateVariableContainer->remove($as);
+        }
         return $output;
 
     }

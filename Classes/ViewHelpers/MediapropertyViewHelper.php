26da3eb9e14e8a3562d06c1de225736a3c74e522
Frank Berger <fberger@b-factor.de>
2016-07-04 14:06:49 +0200

    1594 BKV3 Frontend: Ausgabe Bildunterschriften und Copyright

--- Classes/ViewHelpers/MediapropertyViewHelper.php
+++ Classes/ViewHelpers/MediapropertyViewHelper.php
@@ -15,13 +15,20 @@ class MediapropertyViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractV
     {
         $this->registerArgument('file', 'object', 'The File Reference', true);
         $this->registerArgument('property', 'string', 'The Property to read', true);
+        $this->registerArgument('prepend', 'string', 'Prepend Text', false, '');
+        $this->registerArgument('append', 'string', 'Append Text', false, '');
     }
 
     public function render()
     {
+        $s = '';
         if (get_class($this->arguments['file']) == \TYPO3\CMS\Core\Resource\FileReference::class) {
-            return $this->arguments['file']->getProperty($this->arguments['property']);
+            $s = $this->arguments['file']->getProperty($this->arguments['property']);
         }
-        return '';
+        if (!empty($s)) {
+            if (!empty($this->arguments['prepend'])) $s = $this->arguments['prepend'] . $s;
+            if (!empty($this->arguments['append'])) $s = $s . $this->arguments['append'];
+        }
+        return $s;
     }
 }

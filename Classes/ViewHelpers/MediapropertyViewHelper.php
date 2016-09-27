1c57b0f83053888ed8edeadae00050d4ea536744
Frank Berger <fberger@b-factor.de>
2016-09-27 17:48:28 +0200

    1990 BKV3 (und 4?): MP3-Player - Zeit hängt über Player raus und Copyright verhindert abspielen

--- Classes/ViewHelpers/MediapropertyViewHelper.php
+++ Classes/ViewHelpers/MediapropertyViewHelper.php
@@ -26,8 +26,9 @@ class MediapropertyViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractV
         $s = '';
 
         $file = $this->arguments['file'];
-
+        $type = '0';
         if (is_array($file)) {
+            $type = $file['type'];
             $s = $file[$this->arguments['property']];
         } else {
             if (get_class($file) == \TYPO3\CMS\Extbase\Domain\Model\FileReference::class) {
@@ -35,8 +36,10 @@ class MediapropertyViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractV
             }
             if (get_class($file) == \TYPO3\CMS\Core\Resource\FileReference::class) {
                 $s = $file->getProperty($this->arguments['property']);
+                $type = $file->getProperty('type');
             }
         }
+        if ($type=='3') return '';
         if (!empty($s)) {
             if (!empty($this->arguments['prepend'])) $s = $this->arguments['prepend'] . $s;
             if (!empty($this->arguments['append'])) $s = $s . $this->arguments['append'];

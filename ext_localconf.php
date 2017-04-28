3d5c7fb3bac6b764409f3b387f39017de8c8b916
Frank Berger <fberger@b-factor.de>
2017-04-28 13:20:17 +0200

    performance/caching updates

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,22 +6,28 @@ $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php'][
 
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['Sudhaus7Viewhelpers']['RenderPostProcessHook']['0000-' . $_EXTKEY] = 'SUDHAUS7\\Sudhaus7Viewhelpers\\Hooks\\MetaPostProcessHook->attach';
 
+$acachetables = ['sudhaus7viewhelpers_metatags','sudhaus7viewhelpers_cache'];
+foreach ($acachetables as $cachetable) {
+	if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][$cachetable])) {
+		$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][$cachetable]=[];
+	}
+	if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][$cachetable]['groups'])) {
+		$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][$cachetable]['groups']=[];
+	}
+	if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][$cachetable]['options'])) {
+		$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][$cachetable]['options']=[];
+	}
+	if (!isset($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][$cachetable]['backend'])) {
+		$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][$cachetable]['backend'] = 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend';
+	}
+	if (!isset($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][$cachetable]['frontend'])) {
+		$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][$cachetable]['frontend'] = 'TYPO3\\CMS\\Core\\Cache\\Frontend\\VariableFrontend';
+	}
 
-if (!is_array($TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['sudhaus7viewhelpers_metatags'])) {
-    $TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['sudhaus7viewhelpers_metatags'] = array(
-        'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
-        'frontend' => 'TYPO3\\CMS\\Core\\Cache\\Frontend\\VariableFrontend',
-        'groups' => array('pages'),
-        'options' => array('defaultLifetime' => 0),
-    );
+	if (!isset($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][$cachetable]['options']['defaultLifetime'])) {
+		$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][$cachetable]['options']['defaultLifetime'] = 0;
+	}
+	$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][$cachetable]['groups'][] = 'pages';
 }
 
-if (!is_array($TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['sudhaus7viewhelpers_cache'])) {
-    $TYPO3_CONF_VARS['SYS']['caching']['cacheConfigurations']['sudhaus7viewhelpers_cache'] = array(
-        'backend' => 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend',
-        'frontend' => 'TYPO3\\CMS\\Core\\Cache\\Frontend\\VariableFrontend',
-        'groups' => array('pages'),
-        'options' => array('defaultLifetime' => 0),
-    );
-}
 

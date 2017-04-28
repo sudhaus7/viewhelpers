91395925e0dd5e5fdc1607bf9e86ab041f7f2eb3
Frank Berger <fberger@b-factor.de>
2017-04-28 14:15:42 +0200

    performance/caching updates

--- ext_localconf.php
+++ ext_localconf.php
@@ -6,28 +6,7 @@ $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_pagerenderer.php'][
 
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['Sudhaus7Viewhelpers']['RenderPostProcessHook']['0000-' . $_EXTKEY] = 'SUDHAUS7\\Sudhaus7Viewhelpers\\Hooks\\MetaPostProcessHook->attach';
 
-$acachetables = ['sudhaus7viewhelpers_metatags','sudhaus7viewhelpers_cache'];
-foreach ($acachetables as $cachetable) {
-	if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][$cachetable])) {
-		$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][$cachetable]=[];
-	}
-	if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][$cachetable]['groups'])) {
-		$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][$cachetable]['groups']=[];
-	}
-	if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][$cachetable]['options'])) {
-		$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][$cachetable]['options']=[];
-	}
-	if (!isset($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][$cachetable]['backend'])) {
-		$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][$cachetable]['backend'] = 'TYPO3\\CMS\\Core\\Cache\\Backend\\Typo3DatabaseBackend';
-	}
-	if (!isset($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][$cachetable]['frontend'])) {
-		$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][$cachetable]['frontend'] = 'TYPO3\\CMS\\Core\\Cache\\Frontend\\VariableFrontend';
-	}
-
-	if (!isset($GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][$cachetable]['options']['defaultLifetime'])) {
-		$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][$cachetable]['options']['defaultLifetime'] = 0;
-	}
-	$GLOBALS['TYPO3_CONF_VARS']['SYS']['caching']['cacheConfigurations'][$cachetable]['groups'][] = 'pages';
-}
 
 
+$defaultConfig = include( \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath( 'sudhaus7_viewhelpers' ) . '/Configuration/DefaultConfiguration.php' );
+$GLOBALS['TYPO3_CONF_VARS'] = array_replace_recursive( $defaultConfig , $GLOBALS['TYPO3_CONF_VARS']);

a815b169ba85f931308db86e63377faf7d5504d9
markus <mhofmann@b-factor.de>
2017-07-17 11:18:04 +0200

    3234 FE: Update auf neues Template

--- Classes/ViewHelpers/String/CountViewHelper.php
+++ Classes/ViewHelpers/String/CountViewHelper.php
@@ -15,9 +15,14 @@ class CountViewHelper extends AbstractViewHelper {
     {
         parent::initializeArguments();
         $this->registerArgument('string','string','The string to be counted', true);
+        $this->registerArgument('invert','bool','Result has to be inverted to an integer',false,false);
+        $this->registerArgument('maxnum','int','The number from which it has to be inverted',false);
     }
     public function render() {
         $theString = $this->arguments['string'];
+        if ($this->arguments['invert']) {
+            return $this->arguments['maxnum'] - strlen($theString);
+        }
         return strlen($theString);
     }
 }

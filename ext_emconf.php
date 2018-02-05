682bbfc3d71d62b4fbb6ba0c0b6ee14a2d27db3b
markus <mhofmann@b-factor.de>
2018-02-05 13:58:00 +0100

    Updates ViewHelpers, dependencies, Version bump

--- ext_emconf.php
+++ ext_emconf.php
@@ -18,10 +18,10 @@ $EM_CONF[$_EXTKEY] = array(
     'clearCacheOnLoad' => 0,
     'lockType' => '',
     'author_company' => '',
-    'version' => '1.0.26',
+    'version' => '1.0.27',
     'constraints' => array(
         'depends' => array(
-            'cms' => '',
+            'typo3' => '6.2.0-8.7.99'
         ),
         'conflicts' => array(
         ),
